4e83785803eccba4ae206eda5535ba943688351c
Suzie Q <suzieqwolf@gmail.com>
2020-04-01 21:06:50 -0600

    Commit last min changes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//<script src="../hang_src_try/try.js"></script>
 // query selector variables go here 👇
 var backToMainBtn = document.querySelector('.back-to-main'); //this button take you back to main page from saved poster page
 var imageInput = document.querySelector('#poster-image-url'); //this is the image input section for make your our poster
@@ -16,6 +17,9 @@ var targetImage = document.querySelector('.poster-img'); //This is the main post
 var targetQuote = document.querySelector('.poster-quote'); //this is the main poster quote
 var targetTitle = document.querySelector('.poster-title'); //this is the main poster title
 var titleInput = document.querySelector('#poster-title'); //this is the input title section for the make you poster page
+var tossOutDiv = document.querySelector('.mini-poster'); //this is the grid page, all 9 mini-posters areas, purpose: doubleclick delete
+var tossOutMini = document.querySelector('.div.mini-poster'); //this is the grid page, all 9 mini-posters areas, purpose: doubleclick delete
+
 var images = [
   "./assets/bees.jpg",
   "./assets/bridge.jpg",
@@ -36,6 +40,7 @@ var images = [
   "./assets/tiger.jpg",
   "./assets/turtle.jpg"
 ];
+
 var titles = [
   "determination",
   "success",
@@ -73,6 +78,7 @@ var titles = [
   "understanding",
   "wisdom"
 ];
+
 var quotes = [
   "Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
   "You are braver than you believe, stronger than you seem and smarter than you think.",
@@ -123,7 +129,7 @@ var savedPosters = [
 ];
 
 var onscreenPoster;
-// var cusPoster = [(imageInput.value, titleInput.value, quoteInput.value)];
+var cusPoster = [(imageInput.value, titleInput.value, quoteInput.value)];
 
 makePosterBtn.addEventListener('click', switchToFormPage);
 savedPostersBtn.addEventListener('click', switchToSavedPosters);
@@ -132,6 +138,7 @@ backToMainBtn.addEventListener('click', switchBackFromSaved);
 showPosterBtn.addEventListener('click', saveInput);
 randomButton.addEventListener('click', goRandom);
 saveMyPosterBtn.addEventListener('click', goSavePoster);
+var tossOutMinis = document.querySelector('.mini-poster')
 
 function goRandom() {
   var randomImage = images[getRandomIndex(images)];
@@ -163,13 +170,13 @@ function saveInput(event) {
   targetImage.src = cusPoster.imageURL;
   targetTitle.innerText = cusPoster.title;
   targetQuote.innerText = cusPoster.quote;
-  
+
   //added code below
   onscreenPoster = new Poster (cusImage, cusTitle, cusQuote);
-  
+
   if (cusPoster.imageURL === "") {
     switchBackFromSaved();
-    goRandom();  
+    goRandom();
   }
   // added code above
 
@@ -189,9 +196,9 @@ function onlyOnce() {
 function goSavePoster() {
   if (onlyOnce()) {
     savedPosters.push(onscreenPoster);
-    images.push(cusImage.value);
-    titles.push(cusTitle.value);
-    quotes.push(cusQuote.value);    
+    images.push(onscreenPoster.imageInput)
+    titles.push(onscreenPoster.title);
+    quotes.push(onscreenPoster.quote);
   }
 }
     // savedPosters.push(cusPoster) || savedPosters.push(goRandom.value);
@@ -220,8 +227,8 @@ function switchToSavedPosters() {
               <h2>${savedPosters[i].title}</h2>
               <h4>${savedPosters[i].quote}</h4>
             </div>`)
-    } 
-   
+    }
+
 }
         //   <article>
         //     <img src=${savedPosters[i].imageURL}>
@@ -232,9 +239,19 @@ function switchToSavedPosters() {
 //makePoster(targetImage.src, targetTitle.innerText, targetQuote.innerText)
 
 function deletePoster() {
-
-}
-
+  //
+  // for (var i = 0; i < savedPosters.length; i++) {
+  //     //savedPostersGrid.insertAdjacentHTML("afterbegin", `
+  //         <div class="mini-poster" data-id=${savedPosters[i].id}>
+  //           <img src=${savedPosters[i].imageURL}>
+  //           <h2>${savedPosters[i].title}</h2>
+  //           <h4>${savedPosters[i].quote}</h4>
+  //         </div>`)
+  //
+  // var tossOutDiv = document.querySelector('.mini-poster')
+  // var tossOutMini = document.querySelector('.div.mini-poster')
+  //
+} //var savedPostersGrid = document.querySelector('.saved-posters-grid')
 function switchBackFromForm() {
   makePosterSec.classList.add('hidden');
   mainPosterSec.classList.remove('hidden');
